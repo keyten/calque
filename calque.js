5d468518d4479fb54ce66430e42ec403ef78b3cc
Keyten <ikeyten@gmail.com>
2016-01-14 05:55:47 +0300

    Added opening files with file api

--- calque.js
+++ calque.js
@@ -1,206 +1,233 @@
 (function () {
-    function scopeClone(scope) {
-        var newScope = {};
+	function scopeClone(scope) {
+		var newScope = {};
 
-        _.each(scope, function (value, name) {
-            if (value instanceof Function) {
-                newScope[name] = value;
-            } else {
-                newScope[name] = math.clone(value);
-            }
-        });
+		_.each(scope, function (value, name) {
+			if (value instanceof Function) {
+				newScope[name] = value;
+			} else {
+				newScope[name] = math.clone(value);
+			}
+		});
 
-        return newScope;
-    }
+		return newScope;
+	}
 
-    function Calque(inputEl, outputEl) {
-        this.inputEl = inputEl;
-        this.outputEl = outputEl;
+	function Calque(inputEl, outputEl) {
+		this.inputEl = inputEl;
+		this.outputEl = outputEl;
 
-        this.raw = '';
-        this.lines = [];
-        this.expressions = [];
-        this.activeLine = 0;
+		this.raw = '';
+		this.lines = [];
+		this.expressions = [];
+		this.activeLine = 0;
 
-        var handler = function () {
-            this.updateActiveLine();
-            this.input();
+		var handler = function () {
+			this.updateActiveLine();
+			this.input();
 
-            if (this.inputEl.scrollTop !== this.outputEl.scrollTop) {
-                this.outputEl.scrollTop = this.inputEl.scrollTop;
-            }
-        }.bind(this);
+			if (this.inputEl.scrollTop !== this.outputEl.scrollTop) {
+				this.outputEl.scrollTop = this.inputEl.scrollTop;
+			}
+		}.bind(this);
 
-        handler();
+		handler();
 
-        inputEl.onkeydown = handler;
-        inputEl.onkeyup = handler;
-        setInterval(handler, 50);
+		inputEl.onkeydown = handler;
+		inputEl.onkeyup = handler;
+		setInterval(handler, 50);
 
-        outputEl.scrollTop = inputEl.scrollTop;
-        inputEl.onscroll = function () {
-            outputEl.scrollTop = inputEl.scrollTop;
-        };
-    }
+		outputEl.scrollTop = inputEl.scrollTop;
+		inputEl.onscroll = function () {
+			outputEl.scrollTop = inputEl.scrollTop;
+		};
 
-    Calque.prototype.updateActiveLine = function () {
-        var value = this.inputEl.value;
-        var selectionStart = this.inputEl.selectionStart;
+		// files support
+		document.body.addEventListener('drop', this.openFile.bind(this), false);
+		document.body.addEventListener('dragover', function(event){
+			event.stopPropagation();
+			event.preventDefault();
+			event.dataTransfer.dropEffect = 'copy';
+		}, false);
+	}
 
-        var match = value.substr(0, selectionStart).match(/\n/g);
+	Calque.prototype.updateActiveLine = function () {
+		var value = this.inputEl.value;
+		var selectionStart = this.inputEl.selectionStart;
 
-        if (!match) {
-            var activeLine = 1;
-        } else {
-            var activeLine = value.substr(0, selectionStart).match(/\n/g).length + 1;
-        }
+		var match = value.substr(0, selectionStart).match(/\n/g);
 
-        if (this.activeLine !== activeLine) {
-            this.activeLine = activeLine;
-            this.repaint();
-        }
-    }
+		if (!match) {
+			var activeLine = 1;
+		} else {
+			var activeLine = value.substr(0, selectionStart).match(/\n/g).length + 1;
+		}
 
-    Calque.prototype.input = function () {
-        var raw = this.inputEl.value;
-        if (raw !== this.raw) {
-            this.raw = raw;
-            this.lines = this.raw.split("\n");
-            this.recalc();
-        }
-    }
+		if (this.activeLine !== activeLine) {
+			this.activeLine = activeLine;
+			this.repaint();
+		}
+	}
 
-    Calque.prototype.recalc = function () {
-        this.expressions.forEach(function (expression) {
-            expression.line = null;
-        });
+	Calque.prototype.input = function () {
+		var raw = this.inputEl.value;
+		if (raw !== this.raw) {
+			this.raw = raw;
+			this.lines = this.raw.split("\n");
+			this.recalc();
+		}
+	}
 
-        var scope = {
-            last: null
-        };
+	Calque.prototype.recalc = function () {
+		this.expressions.forEach(function (expression) {
+			expression.line = null;
+		});
 
-        this.lines.forEach(function (code, index) {
-            var oldSimilarExpressions = this.expressions.filter(function (expression) {
-                if (expression.line !== null) return;
-                if (expression.code !== code) return;
-                return true;
-            });
+		var scope = {
+			last: null
+		};
 
-            if (oldSimilarExpressions.length) {
-                var expression = oldSimilarExpressions[0];
-                expression.eval(scope);
-            } else {
-                var expression = new Expression(code, scope);
-                this.expressions.push(expression);
-            }
+		this.lines.forEach(function (code, index) {
+			var oldSimilarExpressions = this.expressions.filter(function (expression) {
+				if (expression.line !== null) return;
+				if (expression.code !== code) return;
+				return true;
+			});
 
-            scope = scopeClone(expression.scopeOutput);
+			if (oldSimilarExpressions.length) {
+				var expression = oldSimilarExpressions[0];
+				expression.eval(scope);
+			} else {
+				var expression = new Expression(code, scope);
+				this.expressions.push(expression);
+			}
 
-            if (expression.result !== undefined) {
-                scope.last = expression.result;
-            }
+			scope = scopeClone(expression.scopeOutput);
 
-            expression.line = index;
-        }.bind(this));
+			if (expression.result !== undefined) {
+				scope.last = expression.result;
+			}
 
-        _.remove(this.expressions, { line: null });
+			expression.line = index;
+		}.bind(this));
 
-        this.repaint();
-    };
+		_.remove(this.expressions, { line: null });
 
-    Calque.prototype.repaint = function () {
-        var html = '';
+		this.repaint();
+	};
 
-        this.lines.forEach(function (line, index) {
-            var expression = this.expressions.filter(function (expression) {
-                return expression.line === index;
-            })[0];
+	Calque.prototype.repaint = function () {
+		var html = '';
 
-            if (expression.error) {
-                if (this.activeLine === index + 1) {
-                    var type = 'empty';
-                } else {
-                    var type = 'error';
-                }
-            } else if (expression.result === undefined) {
-                var type = 'empty';
-            } else {
-                var type = 'result';
-            }
+		this.lines.forEach(function (line, index) {
+			var expression = this.expressions.filter(function (expression) {
+				return expression.line === index;
+			})[0];
 
-            var prefix = '';
-            for (var s = 0; s <= expression.code.length; s++) prefix += ' ';
-            if (type === 'empty') prefix += ' ';
-            if (type === 'result') {
-                if (expression.result instanceof Function) {
-                    prefix += 'fn';
-                } else {
-                    prefix += '= ';
-                }
-            }
-            if (type === 'error') prefix += '// ';
+			if (expression.error) {
+				if (this.activeLine === index + 1) {
+					var type = 'empty';
+				} else {
+					var type = 'error';
+				}
+			} else if (expression.result === undefined) {
+				var type = 'empty';
+			} else {
+				var type = 'result';
+			}
 
-            var data = '';
-            if (type === 'result') {
-                if (expression.result === null) {
-                    data = 'null';
-                } else if (expression.result instanceof Function) {
-                    var source = expression.result.toString();
-                    data = '';
-                } else {
-                    data = expression.result.toString();
-                }
-            };
-            if (type === 'error') data = expression.error;
+			var prefix = '';
+			for (var s = 0; s <= expression.code.length; s++) prefix += ' ';
+			if (type === 'empty') prefix += ' ';
+			if (type === 'result') {
+				if (expression.result instanceof Function) {
+					prefix += 'fn';
+				} else {
+					prefix += '= ';
+				}
+			}
+			if (type === 'error') prefix += '// ';
 
-            var lineHtml = '<div class="' + type + '">';
-            lineHtml += '<span class="prefix" data-prefix="' + prefix + '"></span>';
-            lineHtml += '<span class="data">' + data + '</span>';
-            lineHtml += '</div>';
+			var data = '';
+			if (type === 'result') {
+				if (expression.result === null) {
+					data = 'null';
+				} else if (expression.result instanceof Function) {
+					var source = expression.result.toString();
+					data = '';
+				} else {
+					data = expression.result.toString();
+				}
+			};
+			if (type === 'error') data = expression.error;
 
-            html += lineHtml;
-        }.bind(this));
+			var lineHtml = '<div class="' + type + '">';
+			lineHtml += '<span class="prefix" data-prefix="' + prefix + '"></span>';
+			lineHtml += '<span class="data">' + data + '</span>';
+			lineHtml += '</div>';
 
-        this.outputEl.innerHTML = html;
-    };
+			html += lineHtml;
+		}.bind(this));
+
+		this.outputEl.innerHTML = html;
+	};
+
+	Calque.prototype.openFile = function(event){
+		if(!window.File || !window.FileReader || !window.FileList || !window.Blob)
+			return;
+		event.stopPropagation();
+		event.preventDefault();
+
+		var input = this.inputEl;
+		var file = event.dataTransfer.files[0];
+		var reader = new FileReader();
+		reader.onload = function(e){
+			if(input.value.replace(/\s/g, '') !== ''){
+				if(!window.confirm("Are you sure? You\'ll lost your current text."))
+					return;
+			}
+			input.value = e.target.result;
+		};
+		reader.readAsText(file);
+	};
 
 
-    function Expression(code, scope) {
-        this.code = code;
-        this.scopeInput = scopeClone(scope);
-        this.scopeOutput = scopeClone(this.scopeInput);
+	function Expression(code, scope) {
+		this.code = code;
+		this.scopeInput = scopeClone(scope);
+		this.scopeOutput = scopeClone(this.scopeInput);
 
-        try {
-            this.parse = math.parse(code);
+		try {
+			this.parse = math.parse(code);
 
-            this.dependencies = [];
-            this.parse.traverse(function (node) {
-                if (node.isSymbolNode || node.isFunctionNode) {
-                    this.dependencies.push(node.name);
-                }
-            }.bind(this));
+			this.dependencies = [];
+			this.parse.traverse(function (node) {
+				if (node.isSymbolNode || node.isFunctionNode) {
+					this.dependencies.push(node.name);
+				}
+			}.bind(this));
 
-            this.eval(scope);
-        } catch (e) {
-            this.result = null;
-            this.error = e;
-        }
+			this.eval(scope);
+		} catch (e) {
+			this.result = null;
+			this.error = e;
+		}
 
-        this.line = null;
-    };
+		this.line = null;
+	};
 
-    Expression.prototype.eval = function (scope) {
-        this.scopeInput = scopeClone(scope);
-        this.scopeOutput = scopeClone(this.scopeInput);
+	Expression.prototype.eval = function (scope) {
+		this.scopeInput = scopeClone(scope);
+		this.scopeOutput = scopeClone(this.scopeInput);
 
-        try {
-            this.result = this.parse.eval(this.scopeOutput);
-            this.error = null;
-        } catch (e) {
-            this.result = null;
-            this.error = e;
-        }
-    };
+		try {
+			this.result = this.parse.eval(this.scopeOutput);
+			this.error = null;
+		} catch (e) {
+			this.result = null;
+			this.error = e;
+		}
+	};
 
-    window.Calque = Calque;
+	window.Calque = Calque;
 })();
